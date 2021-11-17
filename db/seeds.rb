f48a96948c31318755bd4959c1d3e3de6ce6dff4
Sarah Alexander <sjalex78@gmail.com>
2021-11-17 13:31:14 +1100

    restaurant create show and list all
    coding with Jesse

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address,
+    phone_number:Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+end
+
+
+15.times do
+  Review.create!(
+    content: Faker::Restaurant.review,
+    rating: rand(0..5),
+    restaurant_id: rand(1..5)
+  )
+end
